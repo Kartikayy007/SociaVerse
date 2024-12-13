ca3c13709a1a146377bfc22ee2ce6a26980537de
Kartikay Singh pundir <130781246+kartikay6969@users.noreply.github.com>
2024-12-13 16:04:01 +0530

    feat(backend): implement user authentication with signup and signin routes, add auth middleware, and update package.json

--- Sociaverse/apps/backend/src/app.ts
+++ Sociaverse/apps/backend/src/app.ts
@@ -1,7 +1,7 @@
 import express, { Express } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import routes from './routes';
+import routes from './routes/auth';
 import connectDB from './config/database';
 
 dotenv.config();
@@ -13,7 +13,7 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
-app.use('/', routes);
+app.use('/api/v1', routes);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server running at http://localhost:${port}`);
